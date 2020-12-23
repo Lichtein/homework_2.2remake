722393cf24b3ece36ae6b8902bf1f2b697a0d4ae
Lichtein <72877040+Lichtein@users.noreply.github.com>
2020-12-23 17:49:58 +0600

    Update Ogre.java

--- src/com/company/Ogre.java
+++ src/com/company/Ogre.java
@@ -2,6 +2,10 @@ package com.company;
 
 public class Ogre extends OsnovaXD implements Printable{
 
+    int Age = 37;
+    int Damage = 100;
+    int Rage = 3000;
+
     public Ogre(String name, int HP) {
         super.setName(name);
         super.setHealPoints(HP);
@@ -9,6 +13,6 @@ public class Ogre extends OsnovaXD implements Printable{
 
     @Override
     public void print() {
-        System.out.println("Name: " + getName() + " HP: " + getHealPoints());
+        System.out.println("Name: " + getName() + " HP: " + getHealPoints() + " Age: " + Age + " Damage: " + Damage + " Rage: " + Rage + " sec");
     }
 }
