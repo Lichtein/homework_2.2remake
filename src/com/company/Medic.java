be1091c16e6159640dd9c08ce6aa9d74ac8f5816
Lichtein <72877040+Lichtein@users.noreply.github.com>
2020-12-23 19:52:50 +0600

    Update Medic.java

--- src/com/company/Medic.java
+++ src/com/company/Medic.java
@@ -2,13 +2,40 @@ package com.company;
 
 public class Medic extends OsnovaXD implements Printable{
 
-    int Age = 37;
-    int Damage = 100;
-    int Heal = 250;
+    int Age;
+    int Damage;
+    int Heal;
 
-    public Medic(String name, int HP) {
+    public Medic(String name, int HP, int Age, int Damage, int Heal) {
         super.setName(name);
         super.setHealPoints(HP);
+        setAge(Age);
+        setDamage(Damage);
+        setHeal(Heal);
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public int getDamage() {
+        return Damage;
+    }
+
+    public void setDamage(int damage) {
+        Damage = damage;
+    }
+
+    public int getHeal() {
+        return Heal;
+    }
+
+    public void setHeal(int heal) {
+        Heal = heal;
     }
 
     @Override
