a62014331308e3a78f7cfc3511081d6447e31a19
Lichtein <72877040+Lichtein@users.noreply.github.com>
2020-12-23 20:48:53 +0600

    Update Ogre.java

--- src/com/company/Ogre.java
+++ src/com/company/Ogre.java
@@ -9,34 +9,9 @@ public class Ogre extends OsnovaXD implements Printable{
     public Ogre(String name, int HP, int Age, int Damage, int Rage) {
         super.setName(name);
         super.setHealPoints(HP);
-        setAge(Age);
-        setDamage(Damage);
-        setRage(Rage);
-
-    }
-
-    public int getAge() {
-        return Age;
-    }
-
-    public void setAge(int age) {
-        Age = age;
-    }
-
-    public int getDamage() {
-        return Damage;
-    }
-
-    public void setDamage(int damage) {
-        Damage = damage;
-    }
-
-    public int getRage() {
-        return Rage;
-    }
-
-    public void setRage(int rage) {
-        Rage = rage;
+        this.Age = Age;
+        this.Rage = Rage;
+        this.Damage = Damage;
     }
 
     @Override
