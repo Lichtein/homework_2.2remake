1a7f88ea59411a33a0c8fe090347066d6b650953
Lichtein <72877040+Lichtein@users.noreply.github.com>
2020-12-23 19:55:14 +0600

    Update Warrior.java

--- src/com/company/Warrior.java
+++ src/com/company/Warrior.java
@@ -2,12 +2,40 @@ package com.company;
 
 public class Warrior extends OsnovaXD implements Printable{
 
-    int Age = 37;
-    int Damage = 100;
-    int AngelHeal = 5000;
-    public Warrior(String name, int HP) {
+    int Age;
+    int Damage;
+    int AngelHeal;
+
+    public Warrior(String name, int HP, int Age, int Damage, int AngelHeal) {
         super.setName(name);
         super.setHealPoints(HP);
+        setAge(Age);
+        setDamage(Damage);
+        setAngelHeal(AngelHeal);
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public int getDamage() {
+        return Damage;
+    }
+
+    public void setDamage(int damage) {
+        Damage = damage;
+    }
+
+    public int getAngelHeal() {
+        return AngelHeal;
+    }
+
+    public void setAngelHeal(int angelHeal) {
+        AngelHeal = angelHeal;
     }
 
     @Override
